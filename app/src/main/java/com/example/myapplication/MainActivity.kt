9f12877a03fc48ed0d3dba88d0b35469c10e26be
chong <wenzhe63@gmail.com>
2023-12-04 00:18:46 +0800

    testing change

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Androidd")
+        Greeting("Androiddd")
     }
 }
